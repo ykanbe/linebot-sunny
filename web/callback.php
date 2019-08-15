748e3dec742552babf8691d27273850f60bcbe41
ykanbe <motoki.yayoi@gmail.com>
2019-08-15 11:02:33 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -35,6 +35,38 @@ $email->addTo('hele483cobi@post.wordpress.com')
 //メッセージ以外のときは何も返さず終了
 if($type != "text" && $type != "image"){
 	exit;
+} else {
+  "type": "text", // ①
+  "text": "Select your favorite food category or send me your location!",
+  "quickReply": { // ②
+    "items": [
+      {
+        "type": "action", // ③
+        "imageUrl": "https://example.com/sushi.png",
+        "action": {
+          "type": "message",
+          "label": "Sushi",
+          "text": "Sushi"
+        }
+      },
+      {
+        "type": "action",
+        "imageUrl": "https://example.com/tempura.png",
+        "action": {
+          "type": "message",
+          "label": "Tempura",
+          "text": "Tempura"
+        }
+      },
+      {
+        "type": "action", // ④
+        "action": {
+          "type": "location",
+          "label": "Send location"
+        }
+      }
+    ]
+  }
 }
 
 //メッセージから店舗名を取得
