a5efe5c5e00c2d61a13fcb030ac69a54cfe45891
ykanbe <motoki.yayoi@gmail.com>
2019-08-22 14:13:52 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -237,7 +237,7 @@ if($type == "image"){
 	"altText" => "default",
     "template" => [
         "type" => "buttons",
-		"size" => "md",
+		"size" => "sm",
         "text" => "このアカウントは自動応答のみでのご対応になります。\nはじめにご利用店舗とご利用状況をご選択ください。\n①サニープライズ\n②ハッピーサニーショップ",
         "actions" => [
             [
